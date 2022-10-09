809884d24ef264be0bfc36f69f6ea9b913377fce
zhengdongyuan <zhengdongyuan@hanyangtech.cn>
2022-10-09 10:24:45 +0800

    add SpringBeanPostProcessor

---
+++ rpc-framework-simple/src/main/java/github/javaguide/spring/SpringBeanPostProcessor.java
@@ -0,0 +1,55 @@
+package github.javaguide.spring;
+
+import github.javaguide.annotation.RpcReference;
+import github.javaguide.config.RpcServiceConfig;
+import github.javaguide.extension.ExtensionLoader;
+import github.javaguide.factory.SingletonFactory;
+import github.javaguide.provider.ServiceProvider;
+import github.javaguide.remoting.transport.netty.client.NettyRpcClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Field;
+
+/**
+ * @Description TODO
+ * @Author zhengdongyuan@hanyangtech.cn
+ * @Date 2022/10/9 10:09
+ */
+@Slf4j
+@Component
+public class SpringBeanPostProcessor implements BeanPostProcessor {
+
+    private final NettyRpcClient nettyRpcClient;
+    private final ServiceProvider serviceProvider;
+
+    public SpringBeanPostProcessor(){
+        this.nettyRpcClient = SingletonFactory.getInstance(NettyRpcClient.class);
+        this.serviceProvider = ExtensionLoader.getExtensionLoader(ServiceProvider.class).getExtension("zk");
+    }
+
+
+
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        Class<?> beanClass = bean.getClass();
+        Field[] declaredFields = beanClass.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            if (declaredField.isAnnotationPresent(RpcReference.class)){
+                RpcReference annotation = declaredField.getAnnotation(RpcReference.class);
+                RpcServiceConfig rpcServiceConfig = RpcServiceConfig.builder().group(annotation.group()).version(annotation.version()).build();
+
+
+            }
+        }
+        return bean;
+    }
+}
